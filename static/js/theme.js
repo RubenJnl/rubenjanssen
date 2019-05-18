0f7d1c6fb8d91db9aae5b9b555a3d5fe06b07b8d
RubenJnl <ruben@ubenjanssen.nl>
2019-05-18 12:30:50 +0200

    ↔ Merge Appearance branch with dev

--- static/js/theme.js
+++ static/js/theme.js
@@ -1,15 +1,22 @@
 import * as LS from './localstorage.js';
 
 const theme = () => {
-  getUserSetting();
-  getSetting();
+  replaceCurrent();
+  // getUserSetting();
   bind();
 }
 
+const replaceCurrent = () => {
+  if (LS.getLocalStorage('theme') === 'light' || LS.getLocalStorage('theme') === 'dark'){
+    LS.removeLocalStorage('theme');
+  }
+  getUserSetting();
+}
+
 const getUserSetting = () => {
-  if (LS.getLocalStorage('theme') === 'light'){
+  if (LS.getLocalStorage('theme') === 'swap'){
     let bodyEl = document.getElementsByTagName('html');
-    bodyEl[0].classList.add('light');
+    bodyEl[0].classList.add('swap');
     let switchEl = document.getElementById('theme');
     switchEl.checked = true;
   }
@@ -19,9 +26,9 @@ const bind = () => {
   let switchEl = document.getElementById('theme');
   switchEl.addEventListener('change', (e) => {
     let bodyEl = document.getElementsByTagName('html');
-    bodyEl[0].classList.toggle('light');
+    bodyEl[0].classList.toggle('swap');
     if (switchEl.checked){
-      LS.setLocalStorage('theme', 'light');
+      LS.setLocalStorage('theme', 'swap');
     } else {
       LS.removeLocalStorage('theme');
     }
